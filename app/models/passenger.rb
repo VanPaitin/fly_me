d39dc451b9b030570c0c7c11cdf520f921516205
Mayowa Pitan <mayowa.pitan@andela.com>
2016-04-01 02:20:33 +0100

    Feature (Mailer): Implement open_letter

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,8 +1,9 @@
 class Passenger < ActiveRecord::Base
+  VALID_NAME_REGEX = /\A\D+/
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true
+  VALID_PHONE_REGEX = /\A(\+)?(\s|\d|-)+\z/
+  validates :name, presence: true, format: { with: VALID_NAME_REGEX }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :phone_number, numericality: true
+  validates :phone_number, format: { with: VALID_PHONE_REGEX }
   belongs_to :booking
-  belongs_to :flight
 end
