6b22a38ab8728a852e59eb16a1e0244fc193b42b
Mayowa Pitan <mayowa.pitan@andela.com>
2016-04-05 16:43:22 +0100

    chore (test): complete the testing for the airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,8 +1,4 @@
 class Airport < ActiveRecord::Base
   has_many :from_flights, class_name: "Flight", foreign_key: :from_airport_id
   has_many :to_flights, class_name: "Flight", foreign_key: :to_airport_id
-
-  def to_s
-    "#{name}, #{city}"
-  end
 end
--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Airport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "it should have many fligths" do
+    it "has many departure flights" do
+      is_expected.to have_many(:from_flights).class_name("Flight").
+        with_foreign_key(:from_airport_id)
+    end
+    it "has many destination flights" do
+      is_expected.to have_many(:to_flights).class_name("Flight").
+        with_foreign_key(:to_airport_id)
+    end
+  end
 end
