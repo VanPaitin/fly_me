7dd68d15a0012a75a64b39310613a9b10dcbd819
Mayowa Pitan <mayowa.pitan@andela.com>
2016-04-07 16:51:49 +0100

    chore (specs): complete passenger model spec

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,41 +1,46 @@
 require "rails_helper"
 
 RSpec.describe Passenger, type: :model do
-  let(:passenger) do
-    Passenger.new(name: 'Example User', email: 'user@example.com',
-                  phone_number: '+234 807 930 3489')
-  end
   it { is_expected.to belong_to(:booking) }
+  it "passenger factory must be valid" do
+    passenger = build(:passenger)
+    expect(passenger.valid?).to be true
+  end
   describe ".validates_presence_of name" do
     it "passenger must have a name" do
-      passenger.name = ""
+      passenger = build(:passenger, name: "")
       expect(passenger.valid?).to be false
     end
     it "the name should be valid" do
-      passenger.name = "Tobi1"
+      passenger = build(:passenger, name: "Tobi1")
       expect(passenger.valid?).to be false
+    end
+    it "the name should be valid" do
+      passenger = build(:passenger, name: "Tobi")
+      expect(passenger.valid?).to be true
     end
   end
   describe 'email validations' do
     it 'email must be present' do
-      passenger.email = ''
+      passenger = build(:passenger, email: "")
       expect(passenger.valid?).to be false
     end
     it 'email should not be too long' do
-      passenger.email = 'a' * 244 + '@example.com'
+      passenger = build(:passenger, email: 'a' * 244 + '@example.com')
       expect(passenger.valid?).to be false
     end
     it 'email validation should accept only valid addresses' do
-      valid_address = 'passenger@example.@com'
-      passenger.email = valid_address
+      invalid_address = 'passenger@example.@com'
+      passenger = build(:passenger, email: invalid_address)
       expect(passenger.valid?).to be false
     end
   end
   describe '#before_save' do
     it 'should ensure all emails are downcased at the database level' do
-      passenger.email = 'passenger@EXAMPLE.COM'
+      passenger_email = "PASSENGER@EXAMPLE.COM"
+      passenger = build(:passenger, email: passenger_email)
       passenger.save
-      expect(passenger.email).to eql 'passenger@example.com'
+      expect(passenger.email).to eql passenger_email.downcase
     end
   end
 end
