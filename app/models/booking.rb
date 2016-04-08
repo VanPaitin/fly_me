ae56c86cbc3d6dd34247407c07c7dfd91868b08f
Mayowa Pitan <mayowa.pitan@andela.com>
2016-04-08 15:04:03 +0100

    fix (models): fix passenger name validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,8 @@ class Booking < ActiveRecord::Base
   belongs_to :flight
   belongs_to :user
   has_many :passengers
+  validates_associated :passengers
   accepts_nested_attributes_for :passengers, reject_if: :all_blank,
-                                             allow_destroy: true
+                                 allow_destroy: true
   validates :flight_id, presence: true
 end
